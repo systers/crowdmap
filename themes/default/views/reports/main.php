73fdcd81a13f53189fcccaa58403966e062bccc9
jenpaff <jparak1@sheffield.ac.uk>
2014-08-05 11:57:41 +0200

    T2 Allow Partial Search
    * Add missing </div>

--- themes/default/views/reports/main.php
+++ themes/default/views/reports/main.php
@@ -188,19 +188,21 @@
 						<div class="f-customFields-box">
 
 							<div class="f-custom_fields-box">
+								<?php echo form::open(NULL, array('method' => 'get')); ?>
                                 <div class="report_row" id="custom_field_row_0"><h4><?php echo Kohana::lang('ui_main.keywords'); ?></h4>
                                     <?php print form::input('custom_field_0', '', ' class="text custom_text"'); ?>
                                 </div>
-										 <?php echo $custom_forms_filter; ?>
-                             </div>
+									<?php echo $custom_forms_filter; ?>
+								<?php echo form::close(); ?> 
+                            </div>
 	
-						<?php
-							// Action, allows plugins to add custom filters
-							Event::run('ushahidi_action.report_filters_ui');
-						?>
-					</div>
+							<?php
+								// Action, allows plugins to add custom filters
+								Event::run('ushahidi_action.report_filters_ui');
+							?>
+						</div>
 					<!-- end #accordion -->
-					
+					</div>
 					<div id="filter-controls">
 						<p>
 							<a href="#" class="small-link-button reset" id="reset_all_filters"><?php echo Kohana::lang('ui_main.reset_all_filters'); ?></a> 
